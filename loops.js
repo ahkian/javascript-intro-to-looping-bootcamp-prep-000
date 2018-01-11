b6501fb5f4bac4eab71db58dcd0af34cb1681842
Daniel Brazel <dbrazel4@gmail.com>
2018-01-11 02:12:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ for (var i = 1; i < 100; i++) {
   console.log("Hello World the " + i + " time");
 }
 
-function forLoop(array){
+function forLoop([array]){
   for(let i = 0; i < 26; i++){
     if (i === 1){ array.push("I am 1 strange loop");
   } else {array.push("I am ${i} strange loops")}
